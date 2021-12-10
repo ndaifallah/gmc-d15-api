d77dc34c0459bc1fc2e0e7e9d9c962be013d6340
Ghiles Mekdam <ghilesmekdem@gmail.com>
2021-12-10 23:14:56 +0100

    login done

--- api/controllers/order.js
+++ api/controllers/order.js
@@ -1,3 +1,7 @@
 let orders= require('../models/orders')
 
-let order
+let order= async(req,resp)=>{
+    let name= req.body.name
+    let nickname= req.body.nickname
+    let address= req.body.address
+}
